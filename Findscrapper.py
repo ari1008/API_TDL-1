3285c0bdcbad1bd972f288fc26646edcfe6143ad
Nicolas Debras <nicodebras17@gmail.com>
2022-07-12 22:42:00 +0200

    work but missing desc and error handling

--- Findscrapper.py
+++ Findscrapper.py
@@ -1,5 +1,5 @@
-from posixpath import split
 import requests
+import pprint
 
 
 #decoup la page avec que ce qui nous interesse 
@@ -12,19 +12,38 @@ def parse(content_url):
     ListRes = findElement(list_content_url)
     return ListRes
 
-#recupère url de l'image dans la balise <img>
-def recupUrlIMG(imgStr):
+#Recupère url de l'image dans la balise <img>
+def RecoveryUrlIMG(imgStr):
     list_img = list(imgStr.split(" "))
     for element in list_img:
         if "data-src=" in element:
             return element[element.find("\""):element.rfind("\"")]
+    
+
+#Recupère le title dans la balise <img>
+def Recoverytitle(imgStr):
+    cutStr = imgStr[imgStr.find("alt="):imgStr.find('itemprop=')]
+    return cutStr[5:imgStr.rfind('"')]
+
+
+def RecoveryId(h3str):
+    cutstr = h3str[h3str.find("href="):]
+    tab = list(cutstr.split('>'))
+    return (tab[0])[5:]
+
 
 #recherche information dans élement 
 def findInfoIntoElement(listElement):
     dictInfo = {}
     for i in range(0, len(listElement)):
         if listElement[i] == "    <figure>":
-            dictInfo["URL_INFO"] = recupUrlIMG(listElement[i+1])
+            dictInfo["URL_INFO"] = RecoveryUrlIMG(listElement[i+1])
+            dictInfo["TITLE"] = Recoverytitle(listElement[i+1])
+        if listElement[i] == '      <i class="fa fa-fw fa-map-marker"></i>':
+            dictInfo["PLACE"] = listElement[i+1]
+        if "<h3" in listElement[i]:
+            dictInfo["ID"] = RecoveryId(listElement[i])
+        dictInfo["DESC"] = "a faire"
     return dictInfo
 
 #recherce element
@@ -38,7 +57,7 @@ def findElement(listElement):
             else:
                 ListRes.append(findInfoIntoElement(listElement[first:i]))
                 first = i
-    print(ListRes)
+    printdict(ListRes)
     return ListRes
 
 #prend en paramètre l'url de la page 
@@ -48,9 +67,31 @@ def request(url):
         print("erreur")
         return r.status_code
     else:
-        parse(r.content.decode("utf-8"))
+        return r.content.decode("utf-8")
 
+def printdict(tab):
+    print("Number result : " + str(len(tab)) + "\n")
+    for element in tab:
+        print("========")
+        pprint.pprint(element)
+        print("========")
 
-request("https://www.aladom.fr/aide-personnes-handicapees/paris-75/")
+#a finir
+def locate(cp):
+    return "paris-75/"
+
+#fonction qui contruit l'url 
+def BuildUrl(type, cp):
+    return "https://www.aladom.fr/" + type + "/" + locate(cp)
+
+#fonction qui se lance lors de l'execution de la commande page  
+def Cfind(cp, type):
+    url = BuildUrl(type, cp)
+    cp = request(url)
+    if cp != 400:
+        parse(cp)
+    return
+
+Cfind(78300, "aide-personnes-handicapees")
 
 
