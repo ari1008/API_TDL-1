8b15267ff2cd3af74e9c8d9b703812852230d62f
Nicolas Debras <nicodebras17@gmail.com>
2022-07-13 20:41:49 +0200

    start select

--- Selectscrapper.py
+++ Selectscrapper.py
@@ -1,2 +1,15 @@
-def Cselect():
+from Selectscrapper import request
+
+
+def parse(txtpage):
+    return
+
+def createUrl(id):
+    return "https://www.aladom.fr/" + id
+
+def Cselect(id):
+    url = createUrl(id)
+    txtpage = request(url)
+    if txtpage != 400:
+        parse(txtpage)
     return
